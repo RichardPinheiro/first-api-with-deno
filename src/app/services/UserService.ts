ebf87d5444f18c33ab558f2c7e37ac78afd4ee67
Richard Pinheiro <richardpinheiro1992@gmail.com>
2020-03-06 21:30:31 -0300

    creating a method create in UserService

--- src/app/services/UserService.ts
+++ src/app/services/UserService.ts
@@ -1,6 +1,9 @@
+import { User } from '../models/User';
 
 class UserService {
-    
+    public async create(userData: User): Promise<void> {
+        return await UserRepository.create(userData)
+    }
 }
 
 export default new UserService()
