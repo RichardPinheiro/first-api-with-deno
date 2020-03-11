fa5961649549330a32a5acf985935d7fb8f13d68
Richard Pinheiro <richardpinheiro1992@gmail.com>
2020-03-10 23:58:31 -0300

    creating function update in user service

--- src/app/services/UserService.ts
+++ src/app/services/UserService.ts
@@ -4,6 +4,10 @@ class UserService {
     public async create(userData: User): Promise<void> {
         return await UserRepository.create(userData)
     }
+
+    public async update(userId, userData: User): Promise<void>  {
+        return await UserRepository.update(userData)
+    }
 }
 
 export default new UserService()
